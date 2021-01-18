4ab39f48bd2a638dc06c796a06f5f6308ccd1460
Shohei Jinno <shohei.jinno@gmail.com>
2021-01-18 11:34:21 -0800

    〆 1.5 One Away: Done.

--- src/ch_01.rs
+++ src/ch_01.rs
@@ -24,3 +24,50 @@ pub fn palindfrome_permutation(s: &str) -> bool {
 
     true
 }
+
+// 1.5 One Away
+pub fn is_one_away(s1: &str, s2: &str) -> bool {
+    let (l1, l2) = (s1.len(), s2.len());
+    let rem = l1.rem_euclid(l2);
+
+    // If the remainder of (l1 % l2) is not 1,
+    // l1, or 0, then return false.
+    match rem {
+        r if r == 1 || r == l1 || r == 0 => {}
+        _ => return false,
+    }
+
+    let mut map = HashMap::<char, isize>::new();
+    // Count the number of each character in s1 and
+    // store it in hash map/hash table.
+    s1.chars()
+        .into_iter()
+        .for_each(|c| *map.entry(c).or_insert(0) += 1);
+    println!("BEFORE: {:#?}", map);
+
+    // Iterate through s2 and if char exists in hashmap,
+    // then subtract 1 from the existing count.
+    // If not, increase count by 1.
+    let mut count = 0;
+    for ch in s2.chars() {
+        if let Some(c) = map.get_mut(&ch) {
+            *c -= 1;
+        } else {
+            // *map.entry(ch).or_insert(0) += 1;
+            count += 1;
+        }
+    }
+    println!("AFTER: {:#?}", map);
+    println!("===============");
+
+    // let mut count = 0;
+    for val in map.values() {
+        count += val.abs();
+    }
+
+    if rem == 1 || rem == l1 {
+        return count == 1;
+    } else {
+        return count == 2;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,19 @@ mod ch_01;
 mod ch_10;
 
 fn main() {
-    println!("{}", ch_01::palindfrome_permutation("abcbacdddff"));
+    // println!("{}", ch_01::palindfrome_permutation("abcbacdddff"));
 
-    let arr1 = vec![1, 3, 4, 9, 19, 0, 0, 0, 0, 0, 0, 0, 0];
-    let arr2 = vec![2, 3, 4, 5, 7, 8, 11, 13];
-    println!("{:?}", ch_10::merge_sort(arr1, arr2));
+    // let arr1 = vec![1, 3, 4, 9, 19, 0, 0, 0, 0, 0, 0, 0, 0];
+    // let arr2 = vec![2, 3, 4, 5, 7, 8, 11, 13];
+    // println!("{:?}", ch_10::merge_sort(arr1, arr2));
+
+    let test_cases = [
+        ("pale", "pal"),
+        ("pales", "pale"),
+        ("pale", "bale"),
+        ("pale", "bake"),
+    ];
+    for test in test_cases.iter() {
+        println!("{}", ch_01::is_one_away(test.0, test.1));
+    }
 }
